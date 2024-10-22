6c9eff1fed6f322c7a2a2f3dcada84c31df079f6
piotrbielaszka <50463528+piotrbielaszka@users.noreply.github.com>
2024-10-22 20:57:06 +0200

    Wykonano instrukcje z laboratorium

--- test.cpp
+++ test.cpp
@@ -1,2 +1,26 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+// #define CATCH_CONFIG_MAIN
+#include "Wektor.hpp"
+// #include "catch.hpp"
+#include <iostream>
+
+int main()
+{
+    Wektor w{2};
+    w.set(0, 21);
+    w.set(1, 37);
+    w.print();
+    
+    std::cout << "\n-----\n\n";
+    w.zmienDlugosc(4);
+    w.set(2, 69);
+    w.set(3, 420);
+    w.print();
+
+    std::cout << "\n-----\n\n";
+    w.zmienDlugosc(3);
+    w.print();
+
+    std::cout << "\n-----\n\n";
+    w.zmienDlugosc(4);
+    w.print();
+}
